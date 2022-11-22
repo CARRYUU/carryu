91e516137190be641de2416a1dd854c006641e09
xxrjun <rjun0729@gmail.com>
2022-11-22 13:44:02 +0800

    add `memberModel.js` into `./server/models/`

---
+++ server/models/memberModel.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const memberSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 255,
+  },
+  avatar: Buffer,
+  email: {
+    type: String,
+    required: true,
+    minLength: 6,
+    maxLength: 255,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 255,
+  },
+  role: {
+    type: String,
+    enum: ["student", "instructor"],
+  },
+  shopping_cart: [
+    {
+      course_id: mongoose.Schema.Types.ObjectId,
+    },
+  ],
+  purchase_history: [
+    {
+      course_id: String,
+      price: Number,
+      payment_type: {
+        type: String,
+        enum: ["atm", "points", "credit_card"],
+      },
+      status: {
+        type: String,
+        enum: ["buy", "refund"],
+      },
+    },
+  ],
+  points: int,
+  challenge_record: [
+    {
+      course_id: mongoose.Schema.Types.ObjectId,
+    },
+  ],
+});
+
+module.exports = mongoose.model("Member", memberSchema);
