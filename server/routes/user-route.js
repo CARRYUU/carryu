eea47520acd05dc4d09bcd1219fc01ffbb30b62f
rjun <rjun0729@gmail.com>
2022-12-04 01:14:10 +0800

    feat: add `user-route.js`

---
+++ server/routes/user-route.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  updateUserPassword,
+  deleteUser,
+} = require("../controllers/user-controller");
+const { isAuthenticatedUser, isAdmin } = require("../middlerware/authMiddleware");
+
+router.route("/register").post(registerUser);
+router
+  .route("/profile")
+  .get(isAuthenticatedUser, getUserProfile)
+  .put(isAuthenticatedUser, updateUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updateUserPassword);
+
+module.exports = router;
