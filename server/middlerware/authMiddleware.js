08841f3d054819d92b4c57bcc5ae79bcabf3c8e3
rjun <rjun0729@gmail.com>
2022-12-04 01:12:32 +0800

    feat: add `authMiddleware.js`
    - [x] isAuthenticatedUser
    - [x] isAdmin

---
+++ server/middlerware/authMiddleware.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user-model");
+
+const isAuthenticatedUser = async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded._id).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      return res.status(401).json({
+        err_msg: "Not authorized, token failed.",
+      });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({
+      err_msg: "Not authorized, no token available. Please login first.",
+    });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    return res.status(401).json({
+      err_msg: "Not authorized as an admin.",
+    });
+  }
+};
+
+module.exports = { isAuthenticatedUser, isAdmin };
