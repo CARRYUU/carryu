6e6f4a6c5015f194a0f6036f1813744fbffe8198
rjun <rjun0729@gmail.com>
2022-12-11 02:29:09 +0800

    refactor: add space in auth-route.js

--- server/routes/auth-route.js
+++ server/routes/auth-route.js
@@ -4,6 +4,7 @@ const authCtrl = require("../controllers/auth-controller");
 const router = express.Router();
 
 router.route("/login").post(authCtrl.login);
+
 router.route("/logout").get(authCtrl.logout);
 
 module.exports = router;
