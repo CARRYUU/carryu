2dba786148cce636da7c946f1673b30df5588301
xxrjun <rjun0729@gmail.com>
2022-12-10 19:38:28 +0800

    feat: add functions in `course-controller.js`
    - createNewCourse()
    - getCourseInfoById()
    - getCouseContentById()
    - getCoursesByTitle()

--- server/controllers/course-controller.js
+++ server/controllers/course-controller.js
@@ -1,18 +1,30 @@
 const Course = require("../models/course-model");
+const courseValidation = require("../config/validation").courseValidation;
 
-// @desc    Get all courses
+// @desc    Create new course
 // @route   GET api/course/create
 // @access  Private
-const addNewCourse = (req, res) => {
-  const { name, description, price, image, category, author } = req.body;
+exports.createNewCourse = (req, res) => {
+  // Desctructure the request body
+  let { title, description, price, thumbnail, category } = req.body;
+
+  // Get the user from the request object
+  const user = req.user;
+
+  // Validate the course data.
+  const { error } = courseValidation({ title, price, category });
+  if (error) return res.status(400).json({ err_msg: error.details[0].message });
+
+  // Create new course object and save it into database.
   const course = new Course({
-    name,
+    title,
     description,
     price,
-    image,
+    thumbnail,
     category,
-    author,
+    instructor: user._id,
   });
+
   course
     .save()
     .then((result) => {
@@ -29,11 +41,94 @@ const addNewCourse = (req, res) => {
     });
 };
 
+// @desc    Get course info by id
+// @route   GET api/course/:_id/_info
+// @access  public
+exports.getCourseInfoById = async (req, res) => {
+  const { _id } = req.params;
+
+  const course = await Course.findById(_id);
+
+  if (!course) {
+    return res.status(404).json({
+      msg: "Course not found",
+    });
+  }
+
+  const {
+    title,
+    instructor,
+    description,
+    price,
+    thumbnail,
+    category,
+    views_count,
+    students_count,
+    created,
+  } = course;
+
+  return res.status(200).json({
+    msg: "Course found",
+    course_info: {
+      title,
+      instructor,
+      description,
+      price,
+      thumbnail,
+      category,
+      views_count,
+      students_count,
+      created,
+    },
+  });
+};
+
+// @desc    Get course content by id
+// @route   GET api/course/:_id/content
+// @access  private
+exports.getCourseContentById = async (req, res) => {
+  const { _id } = req.params;
+
+  const course = await Course.findById(_id);
+
+  if (!course) {
+    return res.status(404).json({
+      msg: "Course not found",
+    });
+  }
+
+  return res.status(200).json({
+    msg: "Course found",
+    course,
+  });
+};
+
+// @desc    Get courses by title (fuzzy search)
+// @route   GET api/course/search/:title
+// @access  public
+exports.getCoursesByTitle = async (req, res) => {
+  const { title } = req.params;
+
+  const courses = await Course.find({
+    title: { $regex: title },
+    $orderby: { views_count: -1 },
+  });
+
+  console.log(courses);
+
+  if (!courses) {
+    return res.status(404).json({
+      msg: "No course found",
+    });
+  }
+
+  return res.status(200).json({
+    msg: `Found ${courses.length} courses`,
+    courses,
+  });
+};
+
 // @desc    Delete course
 // @route   GET api/course/delete
 // @access  Private
-const deleteCourseByInstructor = (req, res) => {};
-
-module.exports = {
-  addNewCourse,
-};
+exports.deleteCourseByInstructor = (req, res) => {};
