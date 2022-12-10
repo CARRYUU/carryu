bfa756891a6b8e460e682bcec666cb789c3c104a
rjun <rjun0729@gmail.com>
2022-12-11 02:29:46 +0800

    feat: add isCourseMember in auth-middleware.js

--- server/middlerware/auth-middleware.js
+++ server/middlerware/auth-middleware.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user-model");
+const Course = require("../models/course-model");
 
+// Check if the user is authenticated.
 const isAuthenticatedUser = async (req, res, next) => {
   let token;
 
@@ -9,7 +11,12 @@ const isAuthenticatedUser = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
       req.user = await User.findById(decoded._id).select("-password");
+      if (!req.user) {
+        return res.status(401).json({ err_msg: "User not found." });
+      }
+
       next();
     } catch (error) {
       console.log(error);
@@ -26,6 +33,7 @@ const isAuthenticatedUser = async (req, res, next) => {
   }
 };
 
+// Check if the user is an admin.
 const isAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
@@ -36,6 +44,7 @@ const isAdmin = (req, res, next) => {
   }
 };
 
+// Check if the user is an instructor.
 const isInstructor = (req, res, next) => {
   if (req.user && req.user.isInstructor()) {
     next();
@@ -46,4 +55,35 @@ const isInstructor = (req, res, next) => {
   }
 };
 
-module.exports = { isAuthenticatedUser, isAdmin, isInstructor };
+// Check if the user is a course member, including the instructor or on of students of the course.
+const isCourseMember = async (req, res, next) => {
+  const { _id } = req.params;
+
+  Course.findById(_id)
+    .then((course) => {
+      if (!course) {
+        return res.status(404).json({
+          msg: "Course not found",
+        });
+      }
+
+      if (
+        course.instructor.toString() === req.user._id.toString() ||
+        course.students.includes(req.user._id)
+      ) {
+        next();
+      } else {
+        return res.status(401).json({
+          err_msg: "Only course member can access this course.",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(400).json({
+        err_msg: "Get course failed",
+        error: err,
+      });
+    });
+};
+
+module.exports = { isAuthenticatedUser, isAdmin, isInstructor, isCourseMember };
