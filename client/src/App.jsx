41474f535209dc189b0d79cb95b3588999b077b7
rjun <rjun0729@gmail.com>
2022-12-16 02:14:23 +0800

    feat: add several routes in App.js
    paths
    - "/": HomePage
    - "/course/search": SearchResult
    - "/challenge" : Chanllege
    - "/about": AboutUs
    - "/edit-profile": EditProfile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import "./App.css";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 import Homepage from "./components/HomePage/HomePage";
 import SearchResult from "./components/SearchResult/SearchResult";
 import Challenge from "./components/Challenge/Challenge";
 import EditProfile from "./components/EditProfile/EditProfile";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      {/* <Homepage /> */}
-      <SearchResult />
-      {/* <Challenge /> */}
-      {/* <EditProfile /> */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/course/search" element={<SearchResult />} />
+        <Route path="/challenge" element={<Challenge />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+
+      </Routes>
+      <Footer />
     </div>
   );
 }
