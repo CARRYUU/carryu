de63121258b1f601a8ea99c54400fce125f58c86
rjun <rjun0729@gmail.com>
2022-12-16 02:16:58 +0800

    feat: replace anchor tag to Link component

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Search from "./Search";
 import Nav from "./Nav";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+
+
+
   return (
     <div className="sticky top-0 ">
       <header className="flex backdrop-blur shadow-md py-2 w-full  sticky top-0 flex-wrap items-center bg-gradient-to-t">
-        <a href="/logo" class="flex items-center mb-4 sm:mb-0">
+        <Link to="/" className="flex items-center mb-4 sm:mb-0">
           <img
             src={require("../../icon/logo.png")}
             className="ml-4 mr-0 h-12 flex flex-none"
@@ -16,7 +20,7 @@ const Header = () => {
             src={require("../../icon/name.png")}
             className="text-mainBlue flex flex-none ml-1 w-32"
           ></img>
-        </a>
+        </Link>
         <Search />
         <Nav />
       </header>
