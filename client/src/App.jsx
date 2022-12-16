b95a50da4d1c5fa8f20086fe69dda9f5d8d2af03
carrie57 <carrie91507@gmail.com>
2022-12-16 17:35:39 +0800

    feat: add edit password, login, refister route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,9 @@ import Homepage from "./components/HomePage/HomePage";
 import SearchResult from "./components/SearchResult/SearchResult";
 import Challenge from "./components/Challenge/Challenge";
 import EditProfile from "./components/EditProfile/EditProfile";
+import EditPassword from "./components/EditPassword/EditPassword";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 import AboutUs from "./components/AboutUs/AboutUs";
 
 function App() {
@@ -20,7 +23,9 @@ function App() {
         <Route path="/challenge" element={<Challenge />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/edit-profile" element={<EditProfile />} />
-
+        <Route path="/edit-password" element={<EditPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <Footer />
     </div>
