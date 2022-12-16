c74e4f39f746553dfd407dca22871772463d37c1
xxrjun <rjun0729@gmail.com>
2022-12-16 16:36:16 +0800

    feat: add Provider in `client/index.js`

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import "./index.css";
 import App from "./App";
+
+const store = configureStore({
+  reducer: {},
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </BrowserRouter>
 );
