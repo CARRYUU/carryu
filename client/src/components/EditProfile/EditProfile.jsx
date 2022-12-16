e24725ba414eb1561e7384c53d8eb836f2c69182
carrie57 <carrie91507@gmail.com>
2022-12-16 17:24:53 +0800

    feat: add edit profile page

--- client/src/components/EditProfile/EditProfile.jsx
+++ client/src/components/EditProfile/EditProfile.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Title from "../layout/Title";
+import Button from "../layout/Button";
+import Input from "../layout/Input";
 
 const EditProfile = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Title pageTitle="Profile & Setting" />
+      <div className="p-6 space-y-6">
+        <div className="flex-col">
+          <Input labelName="User name" type="text" placeholder="carrie" />
+          <Input
+            labelName="Email"
+            type="email"
+            placeholder="carrie777@gmail.com"
+          />
+        </div>
+      </div>
+      <div className="mb-8">
+        <Button buttonName="Save all" />
+      </div>
+    </div>
+  );
 };
 
 export default EditProfile;
