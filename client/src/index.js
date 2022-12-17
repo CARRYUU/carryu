c2871932578ea5413d9e957be5df0a9c9c530b8a
rjun <rjun0729@gmail.com>
2022-12-17 17:10:34 +0800

    refactor: replace configureStore with store.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
+import { store } from "./store";
 import "./index.css";
 import App from "./App";
-
-const store = configureStore({
-  reducer: {},
-});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
