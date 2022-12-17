8184e69028e815e13c044a3ff4cdfb399834eb6d
rjun <rjun0729@gmail.com>
2022-12-17 17:03:43 +0800

    feat:  modify Card.jsx fix card height and set description overflow-hidden

--- client/src/components/layout/Card.jsx
+++ client/src/components/layout/Card.jsx
@@ -3,24 +3,28 @@ import React from "react";
 const Card = (props) => {
   return (
     <div>
-      <div className="snap-center card mt-6 ml-6">
+      <div className="snap-center card my-6 mx-6 x-72 h-[440px] ">
         {/* 圖片 */}
         <img
           src={require("../../image/2.JPG")}
           alt="課程圖片"
-          className="object-cover"
+          className="object-cover "
         />
         {/* 內容 */}
-        <div className="p-5 flex-col gap-3 text-left ">
+        <div className="p-5 flex-col gap-3 text-left">
           {/* bedge */}
           <div className="flex items-center gap-2">
-            <span className="badge">{props.badge1}</span>
-            <span className="badge">{props.badge2}</span>
-            <span className="badge">{props.badge3}</span>
+            {props.badge1 && <span className="badge">{props.badge1}</span>}
+            {props.badge2 && <span className="badge">{props.badge2}</span>}
+            {props.badge3 && <span className="badge">{props.badge3}</span>}
           </div>
-          {/* title */}
-          <h2 className="class-title ">{props.vedioName}</h2>{" "}
-          <p className="text-ellipsis"> {props.vedioInfo}</p>
+          {/* title and description*/}
+          <div className="h-[80px]">
+            <h2 className="class-title">{props.title}</h2>{" "}
+            <p className="text-ellipsis overflow-hidden max-h-12">
+              {props.description}
+            </p>{" "}
+          </div>
           {/* price */}
           <span className="text-xl font-bold">${props.salePrice}</span>
           <div className="flex items-center gap-2 mt-1">
