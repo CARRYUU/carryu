751019d1cde2cd92d13e124d30b4e7d7285e3670
rjun <rjun0729@gmail.com>
2022-12-17 16:26:15 +0800

    feat: add localStoreage.js in frontend utils

--- client/src/utils/localStorage.js
+++ client/src/utils/localStorage.js
@@ -1,14 +1,19 @@
 export const addUserToLocalStorage = (user) => {
   localStorage.setItem("user", JSON.stringify(user));
+  console.log("add user to local storage: ", user);
 };
 
 export const removeUserFromLocalStorage = () => {
   localStorage.removeItem("user");
+
+  console.log("remove user from local storage");
 };
 
 export const getUserFromLocalStorage = () => {
   const result = localStorage.getItem("user");
   const user = result ? JSON.parse(result) : null;
+
+  console.log("get user from local storage: ", user);
 
   return user;
 };
