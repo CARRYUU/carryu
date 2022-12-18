e36a9a7b39c76f509d89e34ee3dd55fbdd787696
rjun <rjun0729@gmail.com>
2022-12-18 13:46:08 +0800

    fix: search bugs in Search.jsx

--- client/src/components/layout/Search.jsx
+++ client/src/components/layout/Search.jsx
@@ -1,29 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 
 const Search = () => {
-  const [searchInput, setSearchInput] = useState("");
+  const [searchedText, setSearchedText] = useState("");
 
   const navigate = useNavigate();
 
   const handleChangeInput = (event) => {
     event.preventDefault();
 
-    setSearchInput(event.target.value);
+    setSearchedText(event.target.value);
   };
 
   const handleSubmit = (event) => {
-    navigate(`/courses/search/${searchInput}`);
-  };
+    event.preventDefault();
 
-  // let dataSearch = CourseData.cardData.filter((item) => {
-  //   return Object.keys(item).some((key) =>
-  //     item[key]
-  //       .toString()
-  //       .toLowerCase()
-  //       .includes(filter.toString().toLowerCase())
-  //   );
-  // });
+    navigate(`/course/search/${searchedText}`, {
+      state: { searchedText: searchedText },
+    });
+  };
 
   return (
     <div className="px-6 flex flex-[5_1_400px] items-center relative">
@@ -38,7 +33,7 @@ const Search = () => {
         type="search"
         placeholder="search course..."
         className="ring-mainBlue ring-2 hover:ring-blue-700 focus:ring-inset  block p-1 pl-6 w-80 ud-text-input  ud-text-input-small ud-text-sm ud-search-form-autocomplete-input js-header-search-field shadow-md rounded-xl"
-        value={searchInput}
+        value={searchedText}
         onChange={handleChangeInput}
         name="search"
       />
