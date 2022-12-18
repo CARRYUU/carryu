51ba51aecdfc942de719d6db60a3ffcc5f06f4b5
rjun <rjun0729@gmail.com>
2022-12-18 17:53:46 +0800

    style: totalPrice in TotalPrice.jsx

--- client/src/components/Cart/TotalPrice.jsx
+++ client/src/components/Cart/TotalPrice.jsx
@@ -8,7 +8,7 @@ const TotalPrice = (props) => {
     <div>
       <div id="TotalPrice" className="flex-col m-4 flex-none text-left">
         <h3 className="font-bold text-m text-gray-500">Total:</h3>
-        <h3 className="font-bold text-2xl  my-5 ">NT${props.TotalPrice}680</h3>
+        <h3 className="font-bold text-2xl  my-5 ">NT${props.totalPrice}</h3>
         <Link to="/checkout">
           <Button buttonName="Checkout" className="flex" />
         </Link>
