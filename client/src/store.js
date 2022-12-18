c0d0e724adc0d2a6855b070802005a536f56f630
rjun <rjun0729@gmail.com>
2022-12-18 17:52:26 +0800

    feat: add reducers in client/store.js add courseReducer and cartReducer

--- client/src/store.js
+++ client/src/store.js
@@ -3,11 +3,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
 import userReducer from "./features/user/userSlice";
 import allCoursesReducer from "./features/allCourses/allCoursesSlice";
+import courseReducer from "./features/course/courseSlice";
+import cartReducer from "./features/cart/cartSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     courses: allCoursesReducer,
+    course: courseReducer,
+    cart: cartReducer,
   },
 });
