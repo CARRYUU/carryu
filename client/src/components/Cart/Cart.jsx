7869f2951a139bc774fb75d763a741fe2683e0fe
rjun <rjun0729@gmail.com>
2022-12-18 17:53:11 +0800

    feat: done Cart.jsx and CartItem.jsx rename ShopList.jsx to CartItem.jsx

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,11 +1,23 @@
-import React from "react";
-import Header from "../layout/Header";
-import Footer from "../layout/Footer";
-import Button from "../layout/Button";
-import ShopList from "./ShopList";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import TotalPrice from "./TotalPrice";
+import CartItem from "./CartItem";
+
+import { getCartItems } from "../../features/cart/cartSlice";
 
 const Cart = (props) => {
+  const dispatch = useDispatch();
+
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
+
+  const totalPrice = cartItems?.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
+
   return (
     <div>
       <body className="mx-20">
@@ -17,22 +29,27 @@ const Cart = (props) => {
           <div id="ShopList" className="flex-col flex-1 text-left ">
             <h3 className="font-bold text-xl ">Course in Cart</h3>
             <hr className="mx-10 my-4 h-0.5 bg-gray-100" />
-            <ShopList
-              imageUri={require("../../image/3.JPG")}
-              title="陽明山大爆走"
-              badge1="爬起來"
-              badge2="耶~我誰"
-              price="50000"
-            />
-            <ShopList
-              imageUri={require("../../image/4.JPG")}
-              title="山岳攝影"
-              badge1="相機"
-              badge2="photoshop"
-              price="400"
-            />
+            {cartItems && cartItems.length > 0 ? (
+              cartItems.map((item) => (
+                <CartItem
+                  key={item._id}
+                  _id={item._id}
+                  title={item.title}
+                  price={item.price}
+                  thumbnail={item.thumbnail}
+                  badge1={item.category}
+                  badge2={item.students_count}
+                />
+              ))
+            ) : (
+              <div className="text-center text-gray-500">
+                <h3 className="font-bold text-xl ">
+                  {isLoading ? "Loading..." : "No course in cart"}
+                </h3>
+              </div>
+            )}
           </div>
-          <TotalPrice />
+          <TotalPrice totalPrice={totalPrice} />
         </div>
       </body>
     </div>
