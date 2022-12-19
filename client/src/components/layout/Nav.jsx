b1f099721c2df5a1ab50f583972c3a546e489aa9
rjun <rjun0729@gmail.com>
2022-12-19 22:39:18 +0800

    refactor: codes in Nav.jsx

--- client/src/components/layout/Nav.jsx
+++ client/src/components/layout/Nav.jsx
@@ -1,21 +1,8 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import Dropdown from "./Dropdown";
 
-import userIcon from "../../icon/userIcon.svg";
-// import { ReactComponent as Logo } from "./../icon/userIcon.svg";
-import { logout } from "../../features/auth/authSlice";
-
 const Nav = () => {
-  const dispatch = useDispatch();
-
-  const user = useSelector((state) => state.auth.user);
-
-  const handleLogout = () => {
-    dispatch(logout());
-  };
-
   return (
     <nav className="flex  flex-end  justify-around flex-[2_1_400px] w-32 items-center">
       <ul className="text-mainBlue font-semibold flex flex-end flex-auto w-40 justify-around text-center">
@@ -27,25 +14,9 @@ const Nav = () => {
         </li>
       </ul>
 
-      
       <Dropdown />
-      <Link href="/cart">
-
-
-      {!user && (
-        <Link to="/auth/login">
-          <img src={userIcon} className="p-2"></img>
-        </Link>
-      )}
-
-      {user && (
-        <button onClick={handleLogout}>
-          <img src={userIcon} className="p-2"></img>
-        </button>
-      )}
 
       <Link to="/cart">
-
         <img src={require("../../icon/iconCart.png")} className="p-2"></img>
       </Link>
       <Link to="/language">
