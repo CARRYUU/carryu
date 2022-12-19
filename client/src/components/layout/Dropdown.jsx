6757f602b6e91e66048c531fb899e7a26dd79fb2
rjun <rjun0729@gmail.com>
2022-12-19 22:39:35 +0800

    feat: add conditions nav Dropdown.jsx

--- client/src/components/layout/Dropdown.jsx
+++ client/src/components/layout/Dropdown.jsx
@@ -1,15 +1,27 @@
-import React, { useState, useRef, useSelector, useDispatch } from "react";
+import React, { useState, useRef } from "react";
 import userIcon from "../../icon/userIcon.svg";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+import { logout } from "../../features/auth/authSlice";
 
 const Dropdown = () => {
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.auth.user);
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   const [open, setOpen] = useState(false);
   const Menus = [
     { key: "Login", value: "/auth/login" },
+    { key: "Logout", value: "/auth/logout" },
+    { key: "Profile", value: "/user/profile" },
     { key: "Register", value: "/user/register" },
     { key: "Edit Profile", value: "/user/edit-profile" },
     { key: "Edit Password", value: "/user/edit-password" },
-    { key: "Logout", value: "" }
   ];
 
   const menuRef = useRef();
@@ -38,16 +50,52 @@ const Dropdown = () => {
           >
             <ul>
               <div>
-                {Menus.map((obj) => (
-                  <Link to={obj.value}>
-                    <li
-                      onClick={() => setOpen(false)}
-                      className="p-2 text-la cursor-pointer rounded hover:bg-mainBlue hover:text-white"
-                    >
-                      {obj.key}
-                    </li>
-                  </Link>
-                ))}
+                {Menus.map((obj) => {
+                  if (user && obj.key === "Logout") {
+                    return (
+                      <li
+                        onClick={handleLogout}
+                        className="p-2 text-la cursor-pointer rounded hover:bg-mainBlue hover:text-white"
+                      >
+                        {obj.key}
+                      </li>
+                    );
+                  } else if (
+                    user &&
+                    obj.key !== "Login" &&
+                    obj.key !== "Register"
+                  ) {
+                    return (
+                      <Link to={obj.value}>
+                        <li
+                          onClick={() => setOpen(false)}
+                          className="p-2 text-la cursor-pointer rounded hover:bg-mainBlue hover:text-white"
+                        >
+                          {obj.key}
+                        </li>
+                      </Link>
+                    );
+                  } else if (
+                    !user &&
+                    obj.key !== "Logout" &&
+                    obj.key !== "Profile" &&
+                    obj.key !== "Edit Profile" &&
+                    obj.key !== "Edit Password"
+                  ) {
+                    return (
+                      <Link to={obj.value}>
+                        <li
+                          onClick={() => setOpen(false)}
+                          className="p-2 text-la cursor-pointer rounded hover:bg-mainBlue hover:text-white"
+                        >
+                          {obj.key}
+                        </li>
+                      </Link>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
               </div>
             </ul>
           </div>
