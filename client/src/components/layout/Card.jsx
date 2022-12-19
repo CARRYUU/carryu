8f2db0874371cbf659980381e0e7b5fa964ccfbb
rjun <rjun0729@gmail.com>
2022-12-19 22:38:41 +0800

    refactor: naming in HomePageCourseList.jsx and Card.jsx badge1 -> category badge2 -> students_count

--- client/src/components/layout/Card.jsx
+++ client/src/components/layout/Card.jsx
@@ -15,11 +15,28 @@ const Card = (props) => {
     );
   };
 
+  const {
+    _id,
+    title,
+    salePrice,
+    originalPrice,
+    thumbnail,
+    category,
+    students_count,
+    badge3,
+    description,
+  } = props;
+
   return (
     <div>
       <div className="snap-center card my-6 mx-6 x-72 h-[440px] ">
         {/* 圖片 */}
-        <Link to={`/course/${props._id}/info`} key={props._id}>
+        <Link
+          to={`/course/${_id}/info`}
+          state={{
+            course_id: _id,
+          }}
+        >
           <img
             src={require("../../image/2.JPG")}
             alt="課程圖片"
@@ -31,26 +48,25 @@ const Card = (props) => {
         <div className="p-5 flex-col gap-3 text-left">
           {/* bedge */}
           <div className="flex items-center gap-2">
-            {props.badge1 && <span className="badge">{props.badge1}</span>}
-            {props.badge2 && <span className="badge">{props.badge2}</span>}
-            {props.badge3 && <span className="badge">{props.badge3}</span>}
+            {category && <span className="badge">{category}</span>}
+            {students_count && <span className="badge">{students_count}</span>}
+            {badge3 && <span className="badge">{badge3}</span>}
           </div>
           {/* title and description*/}
           <div className="h-[80px]">
-            <h2 className="class-title">{props.title}</h2>{" "}
+            <h2 className="class-title">{title}</h2>{" "}
             <p className="text-ellipsis overflow-hidden max-h-12">
-              {props.description}
+              {description}
             </p>{" "}
           </div>
           {/* price */}
-          <span className="text-xl font-bold">${props.salePrice}</span>
+          <span className="text-xl font-bold">${salePrice}</span>
           <div className="flex items-center gap-2 mt-1">
             <span className="text-sm line-through opacity-50">
-              ${props.originalPrice}
+              ${originalPrice}
             </span>
             <span className="discount-percent">
-              save{" "}
-              {Math.round((1 - props.salePrice / props.originalPrice) * 100)}%
+              save {Math.round((1 - salePrice / originalPrice) * 100)}%
             </span>
           </div>
           {/* buy button */}
