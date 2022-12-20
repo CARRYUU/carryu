303188b9b29b3473293e164685997634766a6a7b
rjun <rjun0729@gmail.com>
2022-12-20 23:36:02 +0800

    refactor: remove unused code

--- client/src/components/Study/Study.jsx
+++ client/src/components/Study/Study.jsx
@@ -1,20 +1,12 @@
-import Nav from "../layout/Nav";
 import React from "react";
-import Search from "../layout/Search";
-import Header from "../layout/Header";
 import Video from "./Video";
-import Footer from "../layout/Footer";
 import List from "./List";
 
 const Study = () => {
   return (
     <div>
-      <Header />
-      <body>
-        <Video />
-        <List />
-      </body>
-      <Footer />
+      <Video />
+      <List />
     </div>
   );
 };
