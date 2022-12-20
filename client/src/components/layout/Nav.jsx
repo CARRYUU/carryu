18ea11657b64cdeff1e43c3c3fc7dd3d0603ce6f
rjun <rjun0729@gmail.com>
2022-12-20 23:34:59 +0800

    feat: replace with react icons in Nav.jsx

--- client/src/components/layout/Nav.jsx
+++ client/src/components/layout/Nav.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaShoppingCart } from "react-icons/fa";
 import { IoEarthSharp } from "react-icons/io5";
 import Dropdown from "./Dropdown";
 
 const Nav = () => {
   return (
-    <nav className="flex flex-end justify-around flex-[2_1_400px] w-32 items-center">
+    <nav className="flex flex-end justify-around flex-[2_1_400px] w-32 items-center px-4">
       <ul className="text-mainBlue font-semibold flex flex-end flex-auto w-40 justify-around text-center">
         <li className="px-0.1 w-30 ">
           <Link to="/about">關於我們|About us</Link>
@@ -19,7 +19,7 @@ const Nav = () => {
       <Dropdown />
 
       <Link to="/cart">
-        <AiOutlineShoppingCart size={42} className="p-2" />
+        <FaShoppingCart size={42} className="p-2" />
       </Link>
       <Link to="/language">
         <IoEarthSharp size={42} className="p-2" />
