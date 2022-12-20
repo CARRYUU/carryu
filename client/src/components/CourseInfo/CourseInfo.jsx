7ecbb5b3e7e7288cf460ad42aadebda6bf2b4819
rjun <rjun0729@gmail.com>
2022-12-20 21:06:21 +0800

    feat: add CourseInfo.jsx

--- client/src/components/CourseInfo/CourseInfo.jsx
+++ client/src/components/CourseInfo/CourseInfo.jsx
@@ -1,51 +1,121 @@
-import React from "react";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router";
 
-const ClassInformation = () => {
+import { getCourseInfoById } from "../../features/course/courseSlice";
+
+import Title from "../layout/Title";
+import { DiCodeigniter } from "react-icons/di";
+import { BsCurrencyDollar } from "react-icons/bs";
+
+const CourseInfo = (props) => {
+  const dispatch = useDispatch();
+
+  const { courseInfo, isLoading } = useSelector((state) => state.course);
+
+  const {
+    title,
+    instructor,
+    category,
+    description,
+    price,
+    thumbnail,
+    students_count,
+  } = courseInfo?.course_info;
+
+  useEffect(() => {
+    dispatch(getCourseInfoById(props._id));
+  }, []);
+
   return (
     <div>
-      <div
-        name="classinformation"
-        className="h-screen w-full bg-gradient-to-b from-black
-        to-gray-800"
-      >
-        <div
-          className="max-w-screen-lg mx-auto flex flex-col 
-        items-center justify-center h-full px-4 md:flex-row scroll-py-3"
-        >
-          <div className="flex flex-col justify-center h-full ">
-            <h2 className="text-3xl sm:text-5xl font-bold text-white  text-left ">
-              三分鐘笑話速成班
-            </h2>
-            <ls className="text-xl sm:text-2xl font-bold text-white leading-normal pt-3">
-              老師:小蝸
-            </ls>
-            <ls className="text-xl sm:text-2xl font-bold text-white leading-normal pt-1">
-              助教:小白
-            </ls>
-            <p className="text-gray-500 py-4 max-w-md">
-              想學會如何快速講出好聽又吸引人的笑話嗎？挑選這個課程就對了！主豆然豆其
-            </p>
+      {isLoading && (
+        <div className="flex justify-center items-center h-screen">
+          <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+        </div>
+      )}
 
-            <div>
+      <Title pageTitle="課程資訊" />
+
+      <div>
+        <div className="flex ml-40">
+          {/* 圖片 */}
+          <img
+            src={require("../../image/2.JPG")}
+            alt="課程圖片"
+            className=" max-w-md max-h-95  "
+          />
+
+          <div className="  mt-20 ml-40">
+            {/* 類別 */}
+            <div className="text-left  mb-10 text-gray-500">
+              <h1 className="category">
+                類別&gt;
+                {category}
+              </h1>
+            </div>
+
+            {/* 標題 */}
+            <div className="inline-block leading-3 ">
+              <div className="  text-5xl  font-bold text-black  text-left max-w-md overflow-auto h-20 pt-6 ">
+                <h2 className="category">{title}</h2>
+              </div>
+            </div>
+
+            {/* 觀看人數符號 */}
+            <div class="inline-block text-red-600 ">
+              <DiCodeigniter size={21} className="ml-1" />
+            </div>
+
+            {/* 觀看人數 */}
+            <div class="inline-block">
+              <div className=" text-black py-1 max-w-md ">
+                <p className="students_count">{students_count}</p>
+              </div>
+            </div>
+
+            {/* 老師及助教 */}
+            <div className=" text-lg sm:text-2xl font-bold text-black leading-normal pt-3 mt-3">
+              <p className="instructor">
+                {instructor}
+                老師:小蝸
+              </p>
+            </div>
+
+            {/* <div className=" text-lg sm:text-2xl font-bold text-black leading-normal pt-1 mt-3">
+              <p className="instructor">
+                {props.username2}
+                助教:小白
+              </p>
+            </div> */}
+
+            {/* 課程資訊 */}
+            <div className=" text-gray-500 py-4 max-w-lg overflow-auto h-52 mt-3 ">
+              <p className="description">{description}</p>
+            </div>
+
+            {/* 金錢符號 */}
+            <div class="inline-block text-amber-500 h-9 ">
+              <BsCurrencyDollar size={42} className="ml-5 mt-0" />
+            </div>
+
+            {/* 課程價錢 */}
+            <div class="inline-block ">
+              <div className="  text-black text-5xl pt-10  ">
+                <p className="price">{price}</p>
+              </div>
+            </div>
+
+            {/* 購買按鈕 */}
+            <div class="inline-block -mt-3 pt-5 ">
               <button
-                className="group text-white w-fit px-3 py-2 my-2 
-                    flex items-center rounded-md bg-gradient-to-r
-                    from-cyan-500 to-blue-500 cursor-pointer"
+                className="group text-white bg-blue-900 hover:bg-blue-700 
+                                    focus:ring-blue-700 focus:outline-none focus:ring-4 font-bold 
+                                    rounded-full text-xl w-40 h-16 text-center ml-10   "
               >
                 Buy
-                <span className="group-hover:scale-125 duration-200">
-                  <AiOutlineShoppingCart size={21} className="ml-2" />
-                </span>
               </button>
             </div>
-          </div>
-          <div>
-            <img
-              src={require("../../image/1.JPG")}
-              alt="product profile"
-              className="rounded-2xl mx-auto h-auto w-2/3  overflow-visible"
-            />
           </div>
         </div>
       </div>
@@ -53,4 +123,4 @@ const ClassInformation = () => {
   );
 };
 
-export default ClassInformation;
+export default CourseInfo;
