3d7d054e3865c7706d9f0a221e704bf8bba44e15
carrie57 <carrie91507@gmail.com>
2022-12-23 05:08:28 +0800

    feat: upload create course page

--- client/src/components/CreateCourse/CreateCourse.jsx
+++ client/src/components/CreateCourse/CreateCourse.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-import Category from "./Category";
+import CategoryTitle from "./Category";
+import UploadFile from "./UploadFile";
+import Description from "./Description";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-const CreateCourse = () => {
+import Title from "../layout/Title";
+import Button from "../layout/Button";
+import Input from "../layout/Input";
+
+const CreateCourse = (course) => {
   return (
     <div>
-      <Category />
+      <Title pageTitle="Let's create your course!" />
+      <div className="flex flex-col items-center justfiy-center px-4 py-8 mx-auto lg:py-0">
+        <div className="mt-auto flex flex-col justfiy-center w-3/4 py-8 px-2 bg-white rounded-lg shadow dark:bg-gray-100">
+          <div className="px-2/3">
+            <CategoryTitle />
+
+            <Input
+              labelName="Price:"
+              name="price"
+              type="money"
+              placeholder="800"
+              onChange={course.handleChange}
+              value={course.price}
+            />
+            <Description />
+            <UploadFile />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
