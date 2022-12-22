a8b6f56eb38fba344c842e3effb019dc330901ad
carrie57 <carrie91507@gmail.com>
2022-12-23 05:08:06 +0800

    feat: upload course category component

--- client/src/components/CreateCourse/Category.jsx
+++ client/src/components/CreateCourse/Category.jsx
@@ -1,112 +1,90 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-const Category = () => {
+const CategoryTitle = () => {
+  const [open, setOpen] = useState(false);
+  const allCategories = [
+    { key: "web-development" },
+    { key: "programming-language" },
+    { key: "finance" },
+    { key: "it-and-software" },
+    { key: "art-design" },
+    { key: "music" },
+    { key: "system-design" },
+    { key: "music" },
+    { key: "marketing" },
+    { key: "others" }
+  ];
+
+  const menuRef = useRef();
+  const imgRef = useRef();
+
+  window.addEventListener("click", (e) => {
+    if (e.target !== menuRef.current && e.target !== imgRef.current) {
+      setOpen(false);
+    }
+  });
+
   return (
     <div>
-      <form>
-        <div class="flex">
-          <label
-            for="search-dropdown"
-            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      <label className="block mb-2 text-lg font-bold text-gray-900">
+        Course title & category
+      </label>
+      <div className="relative flex justify-center mb-4">
+        <button
+          ref={imgRef}
+          onClick={() => setOpen(!open)}
+          className=" flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-semibold text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 "
+        >
+          category
+          <svg
+            aria-hidden="true"
+            class="w-4 h-4 ml-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            Your Email
-          </label>
-          <button
-            id="dropdown-button"
-            data-dropdown-toggle="dropdown"
-            class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 dark:border-gray-700 dark:text-white rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            type="button"
-          >
-            All categories{" "}
-            <svg
-              aria-hidden="true"
-              class="w-4 h-4 ml-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
+            <path
+              fill-rule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="w-2/3">
+          <input
+            type="text"
+            id="search-dropdown"
+            class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+            placeholder="ex. 30 days become the React master"
+            required
+          ></input>
+        </div>
+        {open && (
           <div
-            id="dropdown"
-            class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700"
+            ref={menuRef}
+            className="absolute top-12 left-16 bg-gray-100 text-mainBlue font-bold p-3 w-48 shadow-lg  "
           >
-            <ul
-              class="py-1 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdown-button"
-            >
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Shopping
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Images
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  News
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Finance
-                </a>
-              </li>
+            <ul>
+              <div>
+                {allCategories.map((obj) => {
+                  return (
+                    <li
+                      onClick=""
+                      className="p-1 text-la cursor-pointer rounded hover:bg-mainBlue hover:text-white"
+                      key={obj.key}
+                    >
+                      {obj.key}
+                    </li>
+                  );
+                })}
+              </div>
             </ul>
           </div>
-          <div class="relative w-full">
-            <input
-              type="search"
-              id="search-dropdown"
-              class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-100 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-              placeholder="Search"
-              required
-            ></input>
-            <button
-              type="submit"
-              class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              <svg
-                aria-hidden="true"
-                class="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </button>
-          </div>
-        </div>
-      </form>
+        )}
+      </div>
     </div>
   );
 };
 
-export default Category;
+export default CategoryTitle;
