5e9837dabd9e5d982b7fdb6f1ce4f900af4e6171
xxrjun <rjun0729@gmail.com>
2022-12-22 13:59:54 +0800

    feat: reduce mb in Input.jsx Checkbox.jsx

--- client/src/components/layout/Input.jsx
+++ client/src/components/layout/Input.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Input = (props) => {
   return (
     <div>
-      <div className="mb-8">
+      <div className="mb-4">
         <label
           className="block mb-2 text-lg font-bold text-gray-900 "
           name={props.name}
