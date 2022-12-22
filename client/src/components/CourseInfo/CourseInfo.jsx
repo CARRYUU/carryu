60ee6f1f0a59ce65930523b6e63422e48fc42d84
Joyyy <108409532@cc.ncu.edu.tw>
2022-12-22 09:46:58 +0800

    排版

--- client/src/components/CourseInfo/CourseInfo.jsx
+++ client/src/components/CourseInfo/CourseInfo.jsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router";
 import { getCourseInfoById } from "../../features/course/courseSlice";
 
 import Title from "../layout/Title";
+import Button from "../layout/Button";
 import { DiCodeigniter } from "react-icons/di";
 import { BsCurrencyDollar } from "react-icons/bs";
 
@@ -18,9 +19,16 @@ const CourseInfo = () => {
   useEffect(() => {
     dispatch(getCourseInfoById(id));
   }, []);
+  
 
-  const { title, category, students_count, instructor, description, price } =
-    courseInfo;
+  const { 
+    title, 
+    category, 
+    students_count, 
+    instructor, 
+    description, 
+    price 
+  } = courseInfo;
 
   return (
     <div>
@@ -32,79 +40,86 @@ const CourseInfo = () => {
 
       {courseInfo && (
         <div>
+          <div className="min-h-screen">
+
           <Title pageTitle="課程資訊" />
 
-          <div>
-            <div className="flex ml-40">
+          <div  >
+            <div className="flex  flex-wrap md:flex-row justify-center  ">
               {/* 圖片 */}
               <img
                 src={require("../../image/2.JPG")}
                 alt="課程圖片"
-                className=" max-w-md max-h-95  "
+                className=" object-cover w-full h-96 rounded-t-lg  md:h-96 md:w-96 md:rounded-none md:rounded-l-lg mt-20"
               />
+            
+              
+              <div className="  sticky top-5 mt-5 ml-5 md:ml-40 flex-col  ">
 
-              <div className="  mt-20 ml-40">
                 {/* 類別 */}
-                <div className="text-left  mb-10 text-gray-500">
+                <div className="  text-left  mb-5 text-gray-500">
                   <h1 className="category">
-                    類別&gt;
+                    類別 &gt;
                     {category}
                   </h1>
                 </div>
 
                 {/* 標題 */}
-                <div className="inline-block leading-3 ">
-                  <div className="  text-5xl  font-bold text-black  text-left max-w-md overflow-auto h-20 pt-6 ">
+                <div className="  inline-block leading-3 ">
+                  <div className="  text-5xl  font-bold text-black  text-left max-w-md h-20 ">
                     <h2 className="category">{title}</h2>
                   </div>
                 </div>
 
                 {/* 觀看人數符號 */}
-                <div class="inline-block text-red-600 ">
+                <div class="  inline-block text-red-600 ">
                   <DiCodeigniter size={21} className="ml-1" />
                 </div>
 
                 {/* 觀看人數 */}
-                <div class="inline-block">
+                <div class="  inline-block">
                   <div className=" text-black py-1 max-w-md ">
                     <p className="students_count">{students_count}</p>
                   </div>
                 </div>
 
                 {/* 老師及助教 */}
-                <div className=" text-lg sm:text-2xl font-bold text-black leading-normal pt-3 mt-3">
+                <div className="  text-lg sm:text-2xl font-bold text-black text-left leading-normal pt-3 mt-3">
                   <p className="instructor">{instructor}</p>
                 </div>
 
                 {/* 課程資訊 */}
-                <div className=" text-gray-500 py-4 max-w-lg overflow-auto h-52 mt-3 ">
+                <div className="  text-gray-500 py-4 max-w-lg overflow-auto text-left h-40 mt-3 ">
                   <p className="description">{description}</p>
                 </div>
 
                 {/* 金錢符號 */}
-                <div class="inline-block text-amber-500 h-9 ">
-                  <BsCurrencyDollar size={42} className="ml-5 mt-0" />
+                <div class="  inline-block text-amber-500 h-9 ">
+                  <BsCurrencyDollar size={42} className="ml-1 " />
                 </div>
 
                 {/* 課程價錢 */}
-                <div class="inline-block ">
-                  <div className="  text-black text-5xl pt-10  ">
+                <div class="  inline-block ">
+                  <div className="  text-black text-5xl pt-10 pr-4 ">
                     <p className="price">{price}</p>
                   </div>
                 </div>
 
-                {/* 購買按鈕 */}
-                <div class="inline-block -mt-3 pt-5 ">
-                  <button
-                    className="group text-white bg-blue-900 hover:bg-blue-700 
-                              focus:ring-blue-700 focus:outline-none focus:ring-4 font-bold 
-                              rounded-full text-xl w-40 h-16 text-center ml-10   "
-                  >
-                    Buy
-                  </button>
+                {/* 立即購買按鈕 */}
+                <div className="   mt-2 mb-2 md:inline-block md:mb-8 ">
+                  <Button buttonName="Buy it Now" /*onClick={}*/ />
                 </div>
+
+                {/* 加入購物車按鈕 */}
+                <div className="  md:inline-block md:mb-8">
+                  <Button buttonName="Add to Cart" /*onClick={}*/ />
+                </div>
+
               </div>
             </div>
+            
+          </div>
+
           </div>
         </div>
       )}
