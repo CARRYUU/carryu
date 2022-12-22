09343bb2b48c58f1704fa1470de22ed12f6a607c
carrie57 <carrie91507@gmail.com>
2022-12-23 01:06:20 +0800

    feat: add env file

---
+++ client/src/components/CreateCourse/CreateCourse.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Category from "./Category";
+
+const CreateCourse = () => {
+  return (
+    <div>
+      <Category />
+    </div>
+  );
+};
+
+export default CreateCourse;
