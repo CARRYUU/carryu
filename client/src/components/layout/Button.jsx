1b233ff4f4c9596cf065f6f0fababfa5075bf160
rjun <rjun0729@gmail.com>
2022-12-23 06:49:08 +0800

    feat: replace mr-2 t omx-2 in Button.jsx

--- client/src/components/layout/Button.jsx
+++ client/src/components/layout/Button.jsx
@@ -5,7 +5,7 @@ const Button = (props) => {
     <div>
       <button
         type="button"
-        className="text-white bg-blue-900 hover:bg-blue-700 focus:ring-blue-700 focus:outline-none focus:ring-4 font-bold rounded-full text-xl w-40 h-16 text-center mr-2 mb-2"
+        className="text-white bg-blue-900 hover:bg-blue-700 focus:ring-blue-700 focus:outline-none focus:ring-4 font-bold rounded-full text-xl w-40 h-16 text-center mx-2 mb-2"
         onClick={props.onClick}
       >
         {props.buttonName}
