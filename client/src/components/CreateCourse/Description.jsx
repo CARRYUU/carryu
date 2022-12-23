374fe9751d958227ffeaeeb95317101030f63a98
carrie57 <carrie91507@gmail.com>
2022-12-23 15:57:50 +0800

    refactor: set RWD

--- client/src/components/CreateCourse/Description.jsx
+++ client/src/components/CreateCourse/Description.jsx
@@ -3,16 +3,18 @@ import React from "react";
 const Description = () => {
   return (
     <div>
-      <div className="px-24 mb-4">
-        <label className="block mb-2 text-lg font-bold text-gray-900">
-          Course description:
-        </label>
-        <textarea
-          id="message"
-          rows="4"
-          className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
-          placeholder="Your course description..."
-        ></textarea>
+      <label className="block mb-2 text-lg font-bold text-gray-900">
+        Course description
+      </label>
+      <div className="mb-4 flex justify-center ">
+        <div className="w-4/5">
+          <textarea
+            id="message"
+            rows="4"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
+            placeholder="Your course description..."
+          ></textarea>
+        </div>
       </div>
     </div>
   );
