e4d2f1ece36f0a69a6a177c6a8b697caa72ff0b2
carrie57 <carrie91507@gmail.com>
2022-12-23 15:57:46 +0800

    refactor: set RWD

--- client/src/components/CreateCourse/CreateCourse.jsx
+++ client/src/components/CreateCourse/CreateCourse.jsx
@@ -15,21 +15,22 @@ const CreateCourse = (course) => {
       <Title pageTitle="Let's create your course!" />
       <div className="flex flex-col items-center justfiy-center px-4 py-8 mx-auto lg:py-0">
         <div className="mt-auto flex flex-col justfiy-center w-3/4 py-8 px-2 bg-white rounded-lg shadow dark:bg-gray-100">
-          <div className="px-2/3">
-            <CategoryTitle />
-
-            <Input
-              labelName="Price:"
-              name="price"
-              type="money"
-              placeholder="800"
-              onChange={course.handleChange}
-              value={course.price}
-            />
-            <Description />
-            <UploadFile />
-            <Button buttonName="Upload" onClick="" />
-          </div>
+          <CategoryTitle
+          // onChange={handleChange}
+          // cayegoryValue={category}
+          // titleValue={title}
+          />
+          <Input
+            labelName="Price"
+            name="price"
+            type="money"
+            placeholder="ex. 800"
+            onChange={course.handleChange}
+            value={course.price}
+          />
+          <Description />
+          <UploadFile />
+          <Button buttonName="Upload" onClick="" />
         </div>
       </div>
     </div>
