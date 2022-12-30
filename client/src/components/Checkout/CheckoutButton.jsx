58ae252ebf13dd44d9bd8df680754d9714d1e5cc
rjun <rjun0729@gmail.com>
2022-12-31 02:26:44 +0800

    feat: done Checkout component API connection

--- client/src/components/Checkout/CheckoutButton.jsx
+++ client/src/components/Checkout/CheckoutButton.jsx
@@ -5,7 +5,8 @@ const CheckoutButton = (props) => {
     <div>
       <button
         type="button"
-        class=" text-white bg-blue-900/80 hover:bg-blue-800  focus:bg-blue-900 font-bold rounded-full text-base w-48 h-14 text-center mr-2 mb-2 px-2"
+        className=" text-white bg-blue-900/80 hover:bg-blue-800  focus:bg-blue-900 font-bold rounded-full text-base w-48 h-14 text-center mr-2 mb-2 px-2"
+        onClick={props.onClick}
       >
         {props.buttonName}
       </button>
--- client/src/components/Checkout/CheckoutItem.jsx
+++ client/src/components/Checkout/CheckoutItem.jsx
@@ -5,19 +5,23 @@ const CheckoutItem = (props) => {
     <div>
       <div className="list flex cardSearch hover:bg-slate-100/60 my-4 max-w-2xl">
         <div className="card w-48">
-          <img src={props.imageUri}></img>
+          <img
+            src={require("../../image/carryu_big_logo_white_background.png")}
+            alt="課程圖片"
+            className="w-48 object-cover h-full"
+          />
         </div>
         <div className="mx-2">
           <h3 className="font-bold text-m m-2">{props.title} </h3>
           <div className="flex ">
-            <div class="middle" className="badge ml-1">
+            <div class="middle" className="badge ml-1 text-xl">
               {props.badge1}
             </div>
-            <div class="middle" className="badge ml-1">
+            <div class="middle" className="badge ml-1 text-xl">
               {props.badge2}
             </div>
           </div>
-          <h3 className="font-bold text-m mx-2 text-bottom">
+          <h3 className="font-bold text-xl mx-2 mt-2 text-bottom">
             NT${props.price}
           </h3>
         </div>
