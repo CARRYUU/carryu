1501aae43c98f2a60e1edb9a4c34ec0e53709b0e
rjun <rjun0729@gmail.com>
2022-12-30 19:16:18 +0800

    feat: done CreateNewCourse - CreateNewCourse.jsx - Description.jsx

--- client/src/components/CreateNewCourse/Description.jsx
+++ client/src/components/CreateNewCourse/Description.jsx
@@ -9,7 +9,8 @@ const Description = (props) => {
       <div className="mb-4 flex justify-center ">
         <div className="w-4/5">
           <textarea
-            id="message"
+            id="description"
+            name="description"
             rows="4"
             className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 "
             placeholder="Your course description..."
