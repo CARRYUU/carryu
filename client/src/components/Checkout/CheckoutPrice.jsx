19682567acf052a04198a3fe1e81644bc3d5caf4
rjun <rjun0729@gmail.com>
2022-12-31 02:26:44 +0800

    feat: done Checkout component API connection

--- client/src/components/Checkout/CheckoutPrice.jsx
+++ client/src/components/Checkout/CheckoutPrice.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import CheckoutButton from "./CheckoutButton";
 
+import { addStudentToCourse } from "../../features/course/courseSlice";
+import { addPurchaseRecord } from "../../features/transaction/transactionSlice";
+import { removeCourseFromCart } from "../../features/cart/cartSlice";
+
 const CheckoutPrice = (props) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { user } = useSelector((state) => state.auth.user);
+  const { transaction_error } = useSelector((state) => state.transaction);
+
   const { cartItems } = props;
 
   let totalPrice = 0;
@@ -11,13 +22,40 @@ const CheckoutPrice = (props) => {
     totalPrice += item.price;
   });
 
+  const handlePurchase = (e) => {
+    e.preventDefault();
+    cartItems.forEach((course) => {
+      dispatch(
+        addStudentToCourse({
+          user_id: user._id,
+          course_id: course._id,
+        })
+      );
+      dispatch(
+        addPurchaseRecord({
+          user_id: user._id,
+          course_id: course._id,
+          payment_method: props.paymentMethod || "ATM",
+        })
+      );
+
+      if (!transaction_error) {
+        dispatch(removeCourseFromCart({ course_id: course._id }));
+      }
+    });
+
+    if (!transaction_error) {
+      navigate("/complete-buy");
+    }
+  };
+
   return (
     <div>
       <div id="totalPrice" className="flex-col m-4 flex-none text-left">
         <div>
           <h3 className="font-bold text-m text-gray-500">Summary:</h3>
           {cartItems.map((item) => (
-            <div>
+            <div key={item._id}>
               <hr className="border-double  mb-4 h-0.5 bg-gray-100 mx-2"></hr>
               <div className="flex m-2">
                 <h4 className="text-sm text-gray-700 font-bold mr-2 w-40">
@@ -32,9 +70,11 @@ const CheckoutPrice = (props) => {
         </div>
         <h3 className="font-bold text-s text-gray-500">Total:</h3>
         <h3 className="font-bold text-2xl  my-5 ">NT${totalPrice}</h3>
-        <Link to="/complete-buy">
-          <CheckoutButton buttonName="Complete Checkout" />
-        </Link>
+
+        <CheckoutButton
+          buttonName="Complete Checkout"
+          onClick={handlePurchase}
+        />
       </div>
     </div>
   );
