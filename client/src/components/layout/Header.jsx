c4d7d5dc85b79907daea916e2af69c2e0675f85a
rjun <rjun0729@gmail.com>
2022-12-30 16:13:27 +0800

    feat: add reloadDocument in logo in Header.jsx

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className="sticky top-0 z-20">
       <header className="flex backdrop-blur shadow-md py-2 w-full sticky top-0 flex-wrap items-center bg-gradient-to-t">
-        <Link to="/" className="flex items-center mb-4 sm:mb-0">
+        <Link to="/" reloadDocument className="flex items-center mb-4 sm:mb-0">
           <SmallLogo className="fill-currentColor ml-4 mr-0 h-12 flex flex-none" />
           <BigLogo className="flex ml-1 w-32" />
         </Link>
