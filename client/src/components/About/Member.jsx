6578a66b96d2b0eb290d37c8f071bba73842f9df
Joyyy <108409532@cc.ncu.edu.tw>
2022-12-31 13:01:20 +0800

    更新list

--- client/src/components/About/Member.jsx
+++ client/src/components/About/Member.jsx
@@ -4,75 +4,44 @@ const Member = () => {
     return (
         <div>
             {/*需要照片*/}
-            <div className=' px-28 py-10 flex flex-row content-center space-x-60'>
+            <div className=' px-20 py-10 flex flex-col  content-center '>
                 <article>
-                    <div className="flex flex-col ">
-                        <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                        <div class=" space-y-1 font-medium ">
-                            <a href="https://github.com/xxrjun">xxrjun </a>
-                        </div>
-                    </div>
-                </article>
-
-                <article>
-                    <div className="flex flex-col ">
-                        <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                        <div class=" space-y-1 font-medium ">
-                            <a href='https://github.com/Tsu-Yu'>Tsu-Yu carrie57 </a>
-                        </div>
-                    </div>
-                </article>
-
-                <article>
-                    <div class="flex items-center mb-4 space-x-4">
-                        <div className="flex flex-col ">
+                    <ul className="flex flex-col md:flex-row  justify-center content-center space-x-40">
+                        <li>
                             <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                            <div class=" space-y-1 font-medium ">
-                                <a href='https://github.com/Chen-LiYin'>Chen-LiYin </a>
-                            </div>
-                        </div>
-                    </div>
+                            <a href="https://github.com/xxrjun" className='space-y-1 font-medium'>xxrjun </a>
+                        </li>
+                        <li>
+                            <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+                            <a href='https://github.com/Tsu-Yu' className=" space-y-1 font-medium ">Tsu-Yu carrie57 </a>
+                        </li>
+                        <li>
+                            <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+                            <a href='https://github.com/Chen-LiYin' className=" space-y-1 font-medium ">Chen-LiYin </a>
+                        </li>
+
+                    </ul>
+
+                    <ul className="flex flex-row  justify-center space-x-40 pt-20">
+                        <li>
+                            <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+                            <a href="https://github.com/heyuhsuan" className='space-y-1 font-medium'>heyuhsuan </a>
+                        </li>
+                        <li>
+                            <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+                            <a href='https://github.com/Elly520' className=" space-y-1 font-medium ">Elly520 </a>
+                        </li>
+                        <li>
+                            <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+                            <a href='https://github.com/HankLiu20' className=" space-y-1 font-medium ">HankLiu20 </a>
+                        </li>
+                        <li>
+                            <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
+                            <a href='https://github.com/Joe-qwe' className=" space-y-1 font-medium ">Joe-qwe </a>
+                        </li>
+
+                    </ul>
                 </article>
-            </div>
-
-            <div className='px-10 py-10 flex flex-row content-center space-x-48'>
-
-                <article>
-                    <div className="flex flex-col ">
-                        <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                        <div class=" space-y-1 font-medium ">
-                            <a href='https://github.com/heyuhsuan'>heyuhsuan </a>
-                        </div>
-                    </div>
-                </article>
-
-                <article>
-                    <div className="flex flex-col ">
-                        <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                        <div class=" space-y-1 font-medium ">
-                            <a href='https://github.com/Elly520' >Elly520 </a>
-                        </div>
-                    </div>
-                </article>
-
-                <article>
-                    <div className="flex flex-col ">
-                        <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                        <div class=" space-y-1 font-medium ">
-                            <a href='https://github.com/HankLiu20' >HankLiu20 </a>
-                        </div>
-                    </div>
-                </article>
-
-                <article>
-                    <div className="flex flex-col ">
-                        <img class=" w-10 h-10 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                        <div class=" space-y-1 font-medium ">
-                            <a href='https://github.com/Joe-qwe' >Joe-qwe </a>
-                        </div>
-                    </div>
-                </article>
-
             </div>
         </div>
     )
