001a1980d2658b46a1007948ade6fd9b63ccb943
Li Yin <lydia0492302209@gmail.com>
2022-12-31 20:16:34 +0800

    add errorimg

--- client/src/components/ErrorPage/ErrorPage.jsx
+++ client/src/components/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { ReactComponent as Errorimg } from "../../icon/404.svg";
-
+import Errorimg from '../../icon/404.svg';
 
 const ErrorPage=()=>{
     return(
         <div className="bg-gradient-to-b from-gray-800 to-black align-middle flex justify-center items-center">
-            
-            <Errorimg className="m-4 flex max-w-2xl  " />
-
+            <img src={Errorimg} className="m-4 flex max-w-2xl" alt="404"/>
         </div>
     )
 }
