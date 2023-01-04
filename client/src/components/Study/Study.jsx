d0e7c88cfafb8417c0f340838aec90327c8e984f
rjun <rjun0729@gmail.com>
2023-01-05 00:26:43 +0800

    feat: done Study mode API connection
    - [ ] comments part

--- client/src/components/Study/Study.jsx
+++ client/src/components/Study/Study.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+
 import Video from "./Video";
 import List from "./List";
 
+import { getCourseContentById } from "../../features/course/courseSlice";
+
 const Study = () => {
+  const dispatch = useDispatch();
+
+  const { courseContent, isLoading } = useSelector((state) => state.course);
+  const courseContentData = courseContent?.course_content;
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    dispatch(getCourseContentById(id));
+  }, []);
+
   return (
     <div>
-      <Video />
-      <List />
+      {isLoading && (
+        <div className="flex justify-center items-center h-screen">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-orange-500"></div>
+        </div>
+      )}
+
+      {courseContent && (
+        <div>
+          <Video />
+          <List courseContentData={courseContentData} />
+        </div>
+      )}
     </div>
   );
 };
