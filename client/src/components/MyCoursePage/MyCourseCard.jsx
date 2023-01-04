ea84bfbcde703a1e69a57de3bb1b2187bba111e5
rjun <rjun0729@gmail.com>
2023-01-05 00:25:38 +0800

    feat: done MyCoursePage.jsx

--- client/src/components/MyCoursePage/MyCourseCard.jsx
+++ client/src/components/MyCoursePage/MyCourseCard.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const MyCourseCard = (props) => {
+  const navigate = useNavigate();
+
+  const { courseId, courseTitle } = props;
+
+  const handleClickToStudy = (e) => {
+    e.preventDefault();
+
+    navigate(`/course/${courseId}/content`);
+  };
+
   return (
     <div>
       <div className="inline-block duration-300 hover:scale-105 motion-reduce:transform-none h-80">
@@ -9,7 +20,8 @@ const MyCourseCard = (props) => {
           <img
             src={require("../../assets/images/carryu_big_logo_gray_background.png")}
             alt="課程圖片"
-            className="object-cover"
+            className="object-cover cursor-pointer"
+            onClick={handleClickToStudy}
           />
           {/* 內容 */}
           <div className="p-5 flex-col gap-3 text-left ">
