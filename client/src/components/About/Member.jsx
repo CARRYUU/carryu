aa1dd1edf2cad2a4a1a54ce7aef4f15a4d89d463
rjun <rjun0729@gmail.com>
2023-01-04 22:42:38 +0800

    feat: done Member.jsx

--- client/src/components/About/Member.jsx
+++ client/src/components/About/Member.jsx
@@ -1,38 +1,31 @@
-import React from 'react'
+import React from "react";
+import { BsGithub } from "react-icons/bs";
 
 const Member = () => {
-    const Menus = [
-        { key: "xxrjun", value: "https://github.com/xxrjun" },
-        { key: "Tsu-Yu", value: "https://github.com/Tsu-Yu" },
-        { key: "Chen-LiYin", value: "https://github.com/Chen-LiYin" },
-        { key: "heyuhsuan", value: "https://github.com/heyuhsuan" },
-        { key: "Elly", value: "https://github.com/Elly520" },
-        { key: "HankLiu", value: "https://github.com/HankLiu20" },
-        { key: "Joe", value: "https://github.com/Joe-qwe" },
-      ];
+  const Menus = [
+    { key: "xxrjun", value: "https://github.com/xxrjun" },
+    { key: "Tsu-Yu", value: "https://github.com/Tsu-Yu" },
+    { key: "Chen-LiYin", value: "https://github.com/Chen-LiYin" },
+    { key: "heyuhsuan", value: "https://github.com/heyuhsuan" },
+    { key: "Elly", value: "https://github.com/Elly520" },
+    { key: "HankLiu", value: "https://github.com/HankLiu20" },
+    { key: "Joe", value: "https://github.com/Joe-qwe" },
+  ];
 
-    return (
-        
-        <div>
-            {/*需要照片*/}
-            <div className=' '>
-                <article>
-                    <ul >
-                        <li>
-                            {Menus.map(item=> (
-                                <li>
-                                    <div className='flex px-20 py-2'>
-                                        <img class=" w-10 h-10 rounded-full " src="/docs/images/people/profile-picture-5.jpg" alt="" />
-                                        <a href = {item.value}>{item.key}</a>
-                                    </div>
-                                </li>
-                            ))}
-                        </li>
-                    </ul>
-                </article>
+  return (
+    <div>
+      <div className="flex flex-wrap px-20">
+        {Menus.map((item) => (
+          <a href={item.value}>
+            <div className="flex py-2 items-center w-60">
+              <BsGithub className="text-2xl mx-2" />
+              <h1 className="text-2xl">{item.key}</h1>
             </div>
-        </div>
-    )
-}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Member
+export default Member;
