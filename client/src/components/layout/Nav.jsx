2b9f70008da7bef95c77e1e40d4e6339929e9e4f
carrie57 <carrie91507@gmail.com>
2023-01-05 12:32:49 +0800

    refactor: import some lose code

--- client/src/components/layout/Nav.jsx
+++ client/src/components/layout/Nav.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import { IoEarthSharp } from "react-icons/io5";
+import { useSelector, useDispatch } from "react-redux";
+import { logout } from "../../features/auth/authSlice";
+import { BsBook } from "react-icons/bs";
+
 import Dropdown from "./Dropdown";
 
 const Nav = () => {
