55f2a2cc1f61e47538358a1864bad2ddd664fe29
carrie57 <carrie91507@gmail.com>
2023-01-05 12:42:06 +0800

    merge conflict

--- client/src/components/Challenge/Category.jsx
+++ client/src/components/Challenge/Category.jsx
@@ -5,7 +5,8 @@ const Roll = (props) => {
     <div className="shadow-black">
       <button
         type="button"
-        class="shadow-lg shadow-gray text-white font-black bg-gradient-to-r from-purple-700 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 rounded-full text-4xl w-96 h-60 text-center mr-2 mb-2"
+        className="shadow-lg shadow-gray text-white font-black bg-gradient-to-r from-purple-700 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 rounded-full text-4xl w-96 h-60 text-center mr-2 mb-2"
+        onClick={props.onClick}
       >
         {props.buttonName}
       </button>
