79840fcb020c17c5f00ab895d5a2c4c4f75e895e
rjun <rjun0729@gmail.com>
2023-01-06 12:38:55 +0800

    feat: done Challenge API connections.

--- client/src/components/Challenge/Roll.jsx
+++ client/src/components/Challenge/Roll.jsx
@@ -2,17 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Roll = (props) => {
+  const { user } = props;
+
   return (
     <div className="min-w-full h-full">
-      <Link to="/confirm-challenge">
-        <button
-          type="button"
-          class="min-w-full h-full shadow-lg shadow-gray text-white font-black bg-gradient-to-r from-orange-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-orange-400 dark:focus:ring-pink-700 rounded-full text-4xl text-center"
-          onClick={props.onClick}
-        >
-          {props.buttonName}
-        </button>
-      </Link>
+      {user && (
+        <Link to="/confirm-challenge">
+          <button
+            type="button"
+            class="min-w-full h-full shadow-lg shadow-gray text-white font-black bg-gradient-to-r from-orange-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-orange-400 dark:focus:ring-pink-700 rounded-full text-4xl text-center"
+            onClick={props.onClick}
+          >
+            {props.buttonName}
+          </button>
+        </Link>
+      )}
+      {!user && (
+        <Link to="/login">
+          <button
+            type="button"
+            class="min-w-full h-full shadow-lg shadow-gray text-white font-black bg-gradient-to-r from-orange-500 to-pink-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-orange-400 dark:focus:ring-pink-700 rounded-full text-4xl text-center"
+            onClick={props.onClick}
+          >
+            {props.buttonName}
+          </button>
+        </Link>
+      )}
     </div>
   );
 };
